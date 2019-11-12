bcf4ba6b0c2486cf29630aa5c2290163aff594c5
sangone88 <llil1@naver.com>
2019-11-12 22:04:19 +0900

    글라이드로 이미지 로드 예제

--- app/src/main/java/com/tjoeun/a20191112_02_library/MainActivity.kt
+++ app/src/main/java/com/tjoeun/a20191112_02_library/MainActivity.kt
@@ -2,6 +2,8 @@ package com.tjoeun.a20191112_02_library
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,10 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        var url = "http://mblogthumb2.phinf.naver.net/20160921_237/ppanppane_1474429787832yFK7y_PNG/%C4%DA%C4%AB%C4%DD%B6%F3_%B7%CE%B0%ED_%283%29.png?type=w800"
+
+        Glide.with(this).load(url).into(profileimg)
 
     }
 
